a71814512f785dfdcc34a04eab7847cddf3eafc2
Ahmet Necdet Yalçın <ahmetnecdetyalcin@Ahmet-MacBook-Air.local>
2023-05-22 19:59:01 +0300

    güncelleme yapıldı

--- src/index.js
+++ src/index.js
@@ -43,3 +43,51 @@ console.log('Proje açıldı!')
 
 
 /* Kodlar Buradan aşağıya */
+let navCount = Object.keys(siteContent.nav).length;
+let j = 1;
+for (let i = 0; i < navCount; i++) {
+  document.querySelectorAll("nav a")[i].classList.add("italic")
+  document.querySelectorAll("nav a")[i].textContent = siteContent.nav["nav-item-" + j]
+  j++
+}
+
+
+document.getElementById("middle-img").src = siteContent.images["accent-img"];
+for (let image in siteContent.images) {
+  if (image === "accent-img") {
+    continue; 
+  }
+  document.getElementById(image).src = siteContent.images[image];
+}
+
+
+document.querySelector(".cta h1").textContent = siteContent.cta["h1"]
+
+document.querySelector(".cta-text button").textContent = siteContent.cta["button"]
+
+
+
+/* Kodlar Buradan aşağıya */
+let icerik = Object.keys(siteContent["ana-içerik"]);
+let k = 0;
+for (let i = 0; i < icerik.length / 2; i++) {
+
+  document.querySelectorAll(".text-content h4")[i].textContent = siteContent["ana-içerik"][icerik[k]];
+  document.querySelectorAll(".text-content p")[i].textContent = siteContent["ana-içerik"][icerik[k+1]];
+  k=k+2;
+  
+}
+
+document.querySelector(".contact h4").textContent = siteContent["iletisim"]["iletişim-h4"];
+
+
+let iletisimk= Object.keys(siteContent.iletisim);
+for (let i=0; i<iletisimk.length-1; i++) {
+ 
+  document.querySelectorAll(".contact p")[i].textContent = siteContent.iletisim[iletisimk[i+1]]
+  
+}
+
+
+document.querySelector("footer a").textContent = siteContent["footer"]["copyright"];
+document.querySelector("footer a").classList.add("bold")
